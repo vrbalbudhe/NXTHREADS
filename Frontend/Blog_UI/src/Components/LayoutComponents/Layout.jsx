7800a8e48106bab2c6e2bcde54f32d61a727cd85
vrbalbudhe <vrbalbudhe@gmail.com>
2025-07-28 01:04:00 +0530

    Layout Updated

--- Frontend/Blog_UI/src/Components/LayoutComponents/Layout.jsx
+++ Frontend/Blog_UI/src/Components/LayoutComponents/Layout.jsx
@@ -6,7 +6,7 @@ import Footer from "./Footer";
 
 export const Layout = () => {
   return (
-    <div className="w-[100%] p-1 md:p-0 scrollable-section md:mb-0 mb-10 h-fit bg-white dark:bg-darkBackground flex flex-col justify-start items-center">
+    <div className="w-[100%] p-1 md:p-0 scrollable-section min-h-screen bg-white dark:bg-darkBackground flex flex-col justify-start items-center">
       <div className="md:w-[80%] top-0 sticky z-50 bg-transparent md:h-fit w-full h-fit">
         <Navbar />
       </div>
@@ -31,7 +31,7 @@ export const RequiredAuth = () => {
   }
 
   return (
-    <div className="w-[100%] h-fit p-1 md:p-0 mb-10 md:mb-0 bg-white dark:bg-darkBackground flex flex-col justify-start items-center">
+    <div className="w-[100%] min-h-screen p-1 md:p-0 bg-white dark:bg-darkBackground flex flex-col justify-start items-center">
       <div className="md:w-[80%] top-0 sticky z-50  md:h-fit w-full h-fit">
         <Navbar />
       </div>
